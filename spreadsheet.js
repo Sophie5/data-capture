30500f8c5a6341548ee4d68d93915e0b1cffe36a
Sophie <sophie.k.mclean@btinternet.com>
2019-06-24 23:22:03 +0100

    read data from a google excel sheet and wrote it into an html page where its's displayed as a table

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,12 +1,23 @@
 var GoogleSpreadSheet = require('google-spreadsheet');
 var creds = require('./client_secret.json/data-capture-8ab6f4aed11a.json');
+var fs = require('fs');
+var tr= "";
 
-var doc = new GoogleSpreadSheet('14m2f6Y2Ue4RkIZGOV8h-1Bn7s6P8ZtjGlrgyO73eCY0');
+  var doc = new GoogleSpreadSheet('14m2f6Y2Ue4RkIZGOV8h-1Bn7s6P8ZtjGlrgyO73eCY0');
 
-doc.useServiceAccountAuth(creds, function(err){
-  doc.getRows(1, function (err, rows){
-    var ds= rows;
-    console.log(ds);
+  doc.useServiceAccountAuth(creds, function (err) {
+    doc.getRows(1, function (err, rows) {
+      rows.forEach(row => {
+        tr += "\n\t\t<tr><td>" + row.country + "</td><td>" + row.product + "</td><td>" + row.profit + "</td></tr>" 
+      });
+      fs.writeFile('table.html', "<link rel='stylesheet' href='table-styling.css'/>\n<table id>" + "\n\t<tbody>\n\t\t<tr><th>Country</th><th>Product</th><th>Profit</th>" +  tr + "\n\t</tbody>\n</table>", function (err) {
+        if (err) throw err;
+        console.log('Saved!');        
+      });
+    });    
   });
-});
+
+
+
+  
 
